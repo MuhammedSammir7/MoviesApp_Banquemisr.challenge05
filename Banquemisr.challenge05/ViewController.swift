a231ec6a472d11eb31f44d1ea4e905d925819aa3
MuhammedSammir7 <mmss2012377@gmail.com>
2024-09-26 22:42:21 +0300

    Initial Commit

---
+++ Banquemisr.challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banquemisr.challenge05
+//
+//  Created by ios on 26/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
