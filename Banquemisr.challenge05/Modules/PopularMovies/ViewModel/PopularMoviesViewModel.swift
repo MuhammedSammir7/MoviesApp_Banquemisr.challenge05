90db1c8cf2a64046e8447842f38b62e28db2ea7b
MuhammedSammir7 <mmss2012377@gmail.com>
2024-09-28 02:59:09 +0300

    displaying the popular movies in popularMovies screen

---
+++ Banquemisr.challenge05/Modules/PopularMovies/ViewModel/PopularMoviesViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  PopularMoviesViewModel.swift
+//  Banquemisr.challenge05
+//
+//  Created by ios on 28/09/2024.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+class PopularMoviesViewModel: MovieViewModelProtocol{
+    private var cancellables = Set<AnyCancellable>()
+    private var networkManager: NetworkManagerProtocol?
+    var urlManager: URLManagerProtocol?
+    
+    @Published var movies: [Movie] = []
+    
+    init() {
+        self.networkManager = NetworkManager()
+        self.urlManager = URLManager()
+    }
+    
+    func fetchNowPlayingMovies() {
+        networkManager?.fetch(url: urlManager?.getUrl(for: .popular) ?? "", type: MoviesResponse.self)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("Successfully fetched movies.")
+                case .failure(let error):
+                    print("Error fetching movies: \(error.localizedDescription)")
+                }
+            }, receiveValue: { [weak self] moviesResponse in
+                guard let result = moviesResponse.results else{
+                    print("Error in response \(URLError(.badServerResponse))")
+                    return
+                    
+                }
+                self?.movies = result
+            })
+            .store(in: &cancellables)
+    }
+    
+    func fetchImage(for movie: Movie) -> AnyPublisher<UIImage?, Never> {
+        let baseUrl = "https://image.tmdb.org/t/p/w500/"
+        
+        guard let imageURL = URL(string: "\(baseUrl)\(movie.posterPath ?? "")")  else {
+                return Just(nil).eraseToAnyPublisher()
+            }
+            
+            return URLSession.shared.dataTaskPublisher(for: imageURL)
+                .map { data, _ in
+                    UIImage(data: data)
+                }
+                .catch { _ in Just(nil) }  // In case of an error, return nil
+                .receive(on: DispatchQueue.main)
+                .eraseToAnyPublisher()
+        }
+
+
+}
